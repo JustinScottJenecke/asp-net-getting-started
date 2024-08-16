4b678d842359636c327a62ff8714d51aeadcd93a
justinscottjenecke <justinjenecke@gmail.com>
2024-08-16 13:09:45 +0200

    feature: generates new ratingColumn migration and updates database

--- 1-razor-pages/RazorMovieApp/Migrations/20240816110224_ratingColumn.Designer.cs
+++ 1-razor-pages/RazorMovieApp/Migrations/20240816110224_ratingColumn.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorMovieApp.Data;
 
@@ -10,9 +11,11 @@ using RazorMovieApp.Data;
 namespace RazorMovieApp.Migrations
 {
     [DbContext(typeof(RazorMovieDbContext))]
-    partial class RazorMovieDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240816110224_ratingColumn")]
+    partial class ratingColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.7");
