f8c1708d2482fbb267a5f62a8d9d98f36923cb72
justinscottjenecke <justinjenecke@gmail.com>
2024-08-16 15:38:10 +0200

    feature: updates Movie model data annotations + creates new migration + completes MS tutorial on Razor Pages

--- 1-razor-pages/RazorMovieApp/Models/Movie.cs
+++ 1-razor-pages/RazorMovieApp/Models/Movie.cs
@@ -10,16 +10,26 @@ public class Movie
     public int Id { get; set; }
     
     [Required]
+    [StringLength(60, MinimumLength = 3)]
     public string Title { get; set; } = string.Empty;
     
-    [DataType(DataType.Date)] [Display (Name = "Release Date")]
+    [DataType(DataType.Date)] 
+    [Display (Name = "Release Date")]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}" /*, ApplyFormatInEditMode = true*/ )]
     public DateTime ReleaseDate { get; set; }
     
     [Required]
+    [StringLength(30)]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
     public string Genre { get; set; } = string.Empty;
-    
-    [Required] [Column(TypeName = "decimal(18, 2)")]
+  
+    [Range(1,100)]
+    [Column(TypeName = "decimal(18, 2)")]
+    [DataType(DataType.Currency)]
     public decimal Price { get; set; }
 
+    [Required]
+    [StringLength(5)]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
     public string Rating {get; set;} = string.Empty;
 }
