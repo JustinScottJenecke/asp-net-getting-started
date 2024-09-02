7bad6e9f1e3b47b01f7cff2f44c254bdebe5d30b
justinscottjenecke <justinjenecke@gmail.com>
2024-09-02 22:39:20 +0200

    feat: creates model class for Movie entities

---
+++ 2-mvc/MvcMovie/Models/Movie.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    [Key]
+    public int Id {get; set;}
+    
+    public string ? Title {get; set;}
+
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate {get; set;}
+
+    public string ? Genre {get; set;}
+
+    public decimal Price {get; set;}
+}
