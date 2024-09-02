9167d861f0532e58914b207506942a219d3a271e
justinscottjenecke <justinjenecke@gmail.com>
2024-09-02 13:05:10 +0200

    feat: creates first razor view for index action

--- 2-mvc/MvcMovie/Controllers/HelloWorldController.cs
+++ 2-mvc/MvcMovie/Controllers/HelloWorldController.cs
@@ -11,9 +11,10 @@ namespace MvcMovie.Controllers
         //     return View();
         // }
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action.";
+            // return "This is my default action.";
+            return View();
         }
 
         public string Welcome(string name, int numTimes = 1)
